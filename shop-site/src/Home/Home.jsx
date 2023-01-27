10de77239bda5dd41e4f27882ceb3d0c50848c71
Nathan Seo <seonathankk@gmail.com>
2023-01-26 20:42:03 -0800

    Change some visual elements and add nav bar buttons on home page

--- shop-site/src/Home/Home.jsx
+++ shop-site/src/Home/Home.jsx
@@ -2,31 +2,32 @@ export default function Home() {
   return (
     <div className="home-page">
       <div className="nav-bar">
+        <button className="text hardware">Hardware</button>
+        <button className="text news">News</button>
         <img
           src="https://cdn.cloudflare.steamstatic.com/steamdeck/images/sd_logo_gradient_animated.png"
           className="logo"
           alt="steam deck logo"
         ></img>
+
+        <button className="text pricing">Pricing</button>
+        <button className="text cart">Cart</button>
       </div>
       <div className="content">
-        <div className="left">
-          <p className="first-line">YOUR GAMES</p>
-          <p className="second-line">ARE GOING</p>
-          <p className="third-line">PLACES.</p>
-          
-        </div>
-        <div className="right">
-          <img
-            src="https://cdn.arstechnica.net/wp-content/uploads/2022/03/elden-deck.png"
-            className="sd-image"
-            alt="steam deck"
-          ></img>
-          <button type="button" className="buy">
-            Buy now!
-          </button>
-          <p className="delivery-estimate">Expected Delivery Date: 1-2 weeks</p>
-        </div>
+        <p className="price">Prices starting at $399.99!*</p>
+        <p className="price-sub-text">*Prices may vary depending on your region.</p>
+        <img
+          src="https://cdn.arstechnica.net/wp-content/uploads/2022/03/elden-deck.png"
+          className="sd-image"
+          alt="steam deck"
+        ></img>
+        <button type="button" className="buy">
+          Buy now!
+        </button>
+        <p className="delivery-estimate">Expected Delivery Date: 1-2 weeks**</p>
+        <p className="delivery-sub-text">**Expected delivery date may vary depending on stock and demand.</p>
       </div>
+      <img className="chat-agent" src="https://www.mayernetworks.com/wp-content/uploads/2021/07/support-icon.png" alt="support"></img>
     </div>
   );
 }
