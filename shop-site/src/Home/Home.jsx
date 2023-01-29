5a8b6a4ad43b257aa80b293837e376dd3f995527
Nathan Seo <seonathankk@gmail.com>
2023-01-29 13:22:31 -0800

    Add link to github to home page

--- shop-site/src/Home/Home.jsx
+++ shop-site/src/Home/Home.jsx
@@ -15,7 +15,9 @@ export default function Home() {
       </div>
       <div className="content">
         <p className="price">Prices starting at $399.99*</p>
-        <p className="price-sub-text">*Prices may vary depending on your region.</p>
+        <p className="price-sub-text">
+          *Prices may vary depending on your region.
+        </p>
         <img
           src="https://cdn.arstechnica.net/wp-content/uploads/2022/03/elden-deck.png"
           className="sd-image"
@@ -25,9 +27,23 @@ export default function Home() {
           Buy now!
         </button>
         <p className="delivery-estimate">Expected Delivery Date: 1-2 weeks**</p>
-        <p className="delivery-sub-text">**Expected delivery date may vary depending on stock and demand.</p>
+        <p className="delivery-sub-text">
+          **Expected delivery date may vary depending on stock and demand.
+        </p>
       </div>
-      <img className="chat-agent" src="https://www.mayernetworks.com/wp-content/uploads/2021/07/support-icon.png" alt="support"></img>
+      <img
+        className="chat-agent"
+        src="https://www.mayernetworks.com/wp-content/uploads/2021/07/support-icon.png"
+        alt="support"
+      ></img>
+      <div className="my-info">
+        <img
+          className="github-image"
+          src="https://git-scm.com/images/logos/downloads/Git-Icon-White.png"
+          alt="github logo"
+        />
+        <a className="github-text" href="https://github.com/n-hyun-seo/shop-site" target="_blank">n-hyun-seo</a>
+      </div>
     </div>
   );
 }
